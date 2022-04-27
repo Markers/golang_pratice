f4da139372cdc3d5f14ef6a433f3e9d4e0e8afe3
Markers <wizard870@gmail.com>
2022-04-27 23:42:03 +0900

    일단은 안되지만 커밋

--- main.go
+++ main.go
@@ -1,42 +1,68 @@
 package main
+
 import (
-        "net/http"
-        "encoding/json"
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/gorilla/mux"
+
+	// http-swagger middleware
+	// httpSwagger ""
+	_ "golang_pratice/docs"
 )
 
 var users = map[string]*User{}
 
 type User struct {
-        Nickname string `json:"nickname"`
-        Email    string `json:"email"`
+	Nickname string `json:"nickname"`
+	Email    string `json:"email"`
 }
 
 func jsonContentTypeMiddleware(next http.Handler) http.Handler {
-        return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request){
-                rw.Header().Add("Content-Type", "application/json")
-                next.ServeHTTP(rw, r)
-        })
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(rw, r)
+	})
 }
 
+// @title swaggo first Test
+// @version 0.0.1?
+// @description 처음써보는 스웨고~
+// @host localhost:8000
+// @BasePath /users
+
 func main() {
-        mux := http.NewServeMux()
+	// mux := http.NewServeMux()
 
-        userHandler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request){
-                switch r.Method {
-                case http.MethodGet:
-                        json.NewEncoder(rw).Encode(users)
-                case http.MethodPost:
-                        var user User
-                        json.NewDecoder(r.Body).Decode(&user)
+	mux := mux.NewRouter()
+	userHandler := http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			json.NewEncoder(rw).Encode(users)
+		case http.MethodPost:
+			var user User
+			json.NewDecoder(r.Body).Decode(&user)
 
-                        users[user.Email] = &user
+			users[user.Email] = &user
 
-                        json.NewEncoder(rw).Encode(user)
-                }
-                //rw.Write([]byte("hello"))
-        })
+			json.NewEncoder(rw).Encode(user)
+		}
+		//rw.Write([]byte("hello"))
+	})
 
-        mux.Handle("/users", jsonContentTypeMiddleware(userHandler))
-        http.ListenAndServe(":8080", mux)
+	mux.Handle("/users", jsonContentTypeMiddleware(userHandler))
+
+	// 스웨고
+	// mux.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
+	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// http.Redirect(w, r, "/static/"+"swagger/", http.StatusSeeOther)
+	// })
+
+	opts := middleware.SwaggerUIOpts{SpecURL: "/swagger.yaml"}
+	sh := middleware.SwaggerUI(opts, nil)
+	mux.Handle("/docs", sh)
+
+	http.ListenAndServe(":8080", mux)
 
 }
